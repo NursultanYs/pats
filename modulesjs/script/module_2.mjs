61cfbd154b0a10e3116ff2320613cc0a7155e2a0
Nursultan <Nursysmailov@gmail.com>
2023-01-05 13:23:23 +0600

    jsModules

---
+++ modulesjs/script/module_2.mjs
@@ -0,0 +1,23 @@
+// import sumNumbers from './module_1.mjs'
+
+// const res1=sumNumbers(10,5);
+
+// console.log(res1);
+
+
+
+// import{
+//     one,
+//     two
+// } from "./module_1.mjs"
+
+// console.log(one)
+// console.log(two)
+
+import{
+    one as oneRenamed,
+    two
+} from "./module_1.mjs"
+
+console.log(oneRenamed)
+console.log(two)
