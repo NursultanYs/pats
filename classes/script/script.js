d023f30be34587623dd5ce148016137da63d2243
Nursultan <nursysmailovv@gmail.com>
2023-01-12 21:37:10 +0600

    classes#2

--- classes/script/script.js
+++ classes/script/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 class Comment {
     constructor(text){
         this.text=text
@@ -38,4 +40,37 @@ console.log(array.sum());
 console.dir(array.__proto__)
 
 
+
+class Rectangle{
+    constructor(height,width){
+        this.height=height;
+        this.width=width;
+    }
+
+    calcArea(){
+        return this.height*this.width;
+    }
+}
+
+class coloredRectangleWithText extends Rectangle{
+    constructor(height,width,text,bgColor){
+        super(height,width);
+        this.text=text;
+        this.bgColor=bgColor;
+    }
+
+    showMyProps(){
+        console.log(`Text: ${this.text}, цвет: ${this.bgColor}`)
+    }
+}
+
+const div=new coloredRectangleWithText(25,10,'Hello World','red');
+console.log(div.calcArea());
+
+
+const square = new Rectangle(10,20);
+const long = new Rectangle(20,100);
+
+console.log(long.calcArea());
+console.log(square.calcArea());
 
